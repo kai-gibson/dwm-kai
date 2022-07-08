523d9d99c52dbbc435b7bb6ebb3c68cb3cc4649f
Kai Gibson <kai.gib@tuta.io>
2022-07-08 16:41:56 +1000

    init dwm repo

---
+++ autostart.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+dwmblocks &
+sh $HOME/.fehbg &
+picom --experimental-backends &
+lxsession &
+udiskie &
+cbatticon -u 20 -i standard -c "poweroff" -l 15 -r 5 &
+nextcloud --background &
+#nm-applet --no-agent &
+brave &
+logseq &
+kitty &
